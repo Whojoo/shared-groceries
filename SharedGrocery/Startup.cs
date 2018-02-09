998edb2a7e49da7fdb7558ab8ff80f96f334e3f6
Robin de Gier <rdgier@dearnova.nl>
2018-02-09 19:47:15 +0100

    Environment variable setup

--- SharedGrocery/Startup.cs
+++ SharedGrocery/Startup.cs
@@ -6,23 +6,40 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using SharedGrocery.Repositories;
 using SharedGrocery.Repositories.DBContexts;
 
 namespace SharedGrocery
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly ILoggerFactory _loggerFactory;
+        private ILogger<Startup> _logger;
+
+        public Startup(IHostingEnvironment hostingEnvironment, ILoggerFactory loggerFactory)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            _loggerFactory = loggerFactory;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // ReSharper disable once UnusedMember.Global
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            _loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            _loggerFactory.AddDebug(LogLevel.Debug);
+            _logger = _loggerFactory.CreateLogger<Startup>();
+            
             services.AddMvc();
             services.AddDbContext<GroceryDataContext>(opt =>
                 opt.UseSqlServer(Configuration.GetConnectionString("Groceries")));
@@ -38,8 +55,10 @@ namespace SharedGrocery
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // ReSharper disable once UnusedMember.Global
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            _logger?.LogDebug("Configuring app");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
