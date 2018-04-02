59bb24ce83c73307e533093ccb2fa97e0412697a
Robin de Gier <de.gier.robin@gmail.com>
2018-04-02 10:28:32 +0200

    Fixes and other stuff

--- SharedGrocery.Tests/Uaa/Service/AuthenticationServiceTest.cs
+++ SharedGrocery.Tests/Uaa/Service/AuthenticationServiceTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using JWT.Algorithms;
 using JWT.Builder;
 using Microsoft.Extensions.Logging;
 using Moq;
--- SharedGrocery/Common/Authentication/JwtAuthenticationHandler.cs
+++ SharedGrocery/Common/Authentication/JwtAuthenticationHandler.cs
@@ -1,20 +1,54 @@
-﻿using System.Text.Encodings.Web;
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using JWT;
+using JWT.Builder;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SharedGrocery.Common.Config;
+using SharedGrocery.Common.Model;
+using SharedGrocery.Common.Util;
 
 namespace SharedGrocery.Common.Authentication
 {
     public class JwtAuthenticationHandler : AuthenticationHandler<JwtAuthenticationOptions>
     {
-        public JwtAuthenticationHandler(IOptionsMonitor<JwtAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        private readonly ApiConfig _apiConfig;
+        
+        public JwtAuthenticationHandler(IOptionsMonitor<JwtAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, IConfiguration configuration) : base(options, logger, encoder, clock)
         {
+            _apiConfig = new ApiConfig(configuration);
         }
 
         protected override Task<AuthenticateResult> HandleAuthenticateAsync()
         {
-            throw new System.NotImplementedException();
+            var token = Request.Headers.GetBearerAuthorization();
+            if (string.IsNullOrEmpty(token))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("No valid token present"));
+            }
+
+            try
+            {
+                var userContext = new JwtBuilder().GetDefaultJwtConfig(_apiConfig)
+                    .MustVerifySignature()
+                    .Decode<UserContext>(token);
+                Logger.LogInformation($"Verfied user {userContext}");
+                var ticket =
+                    new AuthenticationTicket(ClaimsPrincipal.Current, new AuthenticationProperties(), "Bearer");
+                return Task.FromResult(AuthenticateResult.Success(ticket));
+            }
+            catch (TokenExpiredException)
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Token expired"));
+            }
+            catch (SignatureVerificationException)
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Token has invalid signature"));
+            }
         }
+        
     }
 }
